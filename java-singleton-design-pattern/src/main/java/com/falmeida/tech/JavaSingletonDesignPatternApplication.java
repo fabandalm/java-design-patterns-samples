1413fc342ad81ff6d69a7c989019dbc21db419ee
Almeida <falmeida.tech@gmail.com>
2020-05-19 23:22:55 -0400

    First upload.

---
+++ java-singleton-design-pattern/src/main/java/com/falmeida/tech/JavaSingletonDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.falmeida.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSingletonDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSingletonDesignPatternApplication.class, args);
+	}
+
+}
